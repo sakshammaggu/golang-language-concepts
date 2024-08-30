b6a093cd3e7a83cc9d436768f657f3517c9f2226
sakshammaggu <sakshammaggu80@gmail.com>
2024-08-31 00:34:04 +0530

    arrays in go

---
+++ 07arrays/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("my arrays");
+
+	var arr1=[3]int{1, 2, 3};
+	fmt.Println("arr1:", arr1);
+
+	arr2:=[5]int{4, 5, 6, 7};
+	fmt.Println("arr2:", arr2);
+}
