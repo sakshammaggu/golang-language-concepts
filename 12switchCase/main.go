e6b88dc86ef8faf0dbaf4a70fa0c49818fb748fc
sakshammaggu <sakshammaggu80@gmail.com>
2024-09-11 13:23:56 +0530

    switch case in go

---
+++ 12switchCase/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("switch case in go");
+
+	num:=1;
+
+	switch num {
+		case 0:
+			fmt.Println("num is:", num);
+		case 1:
+			fmt.Println("num is:", num);
+	}
+}
